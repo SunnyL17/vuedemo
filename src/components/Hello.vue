36b1f8e483620b8dd37c00f98533cc06c9e15d0f
SunnyL17 <cliu@stcloudstate.edu>
2017-08-16 15:52:33 -0500

    Lots of changes: front-awesome,redirect pages

--- src/components/Hello.vue
+++ src/components/Hello.vue
@@ -2,14 +2,24 @@
   <div class="hello">
     <h1>{{ msg }}</h1>
     <h2>Essential Links</h2>
+      <p>
+      <i class="fa fa-camera-retro fa-5x"></i>
+    <i class="fa fa-facebook-square fa-5x" aria-hidden="true"></i>
+      </p>
        {{user.input1}}
       <!--This is important for using router-link-->
       <router-link to="/Landing">Landing</router-link>
+    
       <div>
-          <form>
-             <input type="text" v-model='Info.input1' maxlength="30" required='required'>
-       </form>
-      </div>
+        
+             <input type="text" v-model='Info.input1' maxlength="30" required>
+         
+           <input type='file' name="image" @change="filesChange($event.target.files)" v-on:change="previewThumbnail" accept="image/*">
+          <figure>
+             <img v-bind:src='imageSrc'>  
+          </figure>
+       
+       </div>
       <button @click="Preview">Submit</button>
   </div>
 </template>
@@ -20,6 +30,8 @@ export default {
   data () {
     return {
       msg: 'Want to see if it works',
+      imageSrc:undefined,
+      imageData:undefined,
       Info:{
         input1:'',
         input2:''
@@ -33,26 +45,49 @@ export default {
     }  
   },
   mounted:function(){
-    this.Info.input1= this.user.input1  
+    
+    this.Info.input1= this.user.input1,
+    this.imageSrc=this.user.imageSrc
   },
   methods:{
       Preview(){
           
           this.$store.commit('ShareInfo',{
-              input1:this.Info.input1
-
+              input1:this.Info.input1,
+              imageSrc:this.imageSrc
           });
-        
           this.$router.push({path:'/Landing'})          
       },
       computed:{
        user(){
       return this.$store.state.user    
-    }  
+    }
+      },
+    previewThumbnail: function(event) {
+      var input = event.target;
+      if (input.files && input.files[0]) {
+        var reader = new FileReader();
+        var vm = this;
+        reader.onload = function(e) {
+          vm.imageSrc = e.target.result;
+          vm.imageData = input.files[0];
+          console.log(vm.imageData)
+        }
+        reader.readAsDataURL(input.files[0]);
+      }
+    },
+    filesChange(fileList) {
+ 
+      if (!fileList.length) return;
+      this.image = fileList[0];
+      console.log(this.image)
+
+ 
+    }
   }
       
   }
-}
+
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
--- src/vuex/store.js
+++ src/vuex/store.js
@@ -8,12 +8,14 @@ const state = {
         Info:{
         input1:'',
         input2:''
-        }   
+        },
+        imageSrc:''
     }
 }
 const mutations = {
     ShareInfo(state,user){
-     state.user.input1=user.input1    
+     state.user.input1=user.input1
+     state.user.imageSrc=user.imageSrc
     } 
 }
 export default new Vuex.Store({
