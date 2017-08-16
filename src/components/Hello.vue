28a728a7870689145d4add67a5688a7c9b63fc00
SunnyL17 <cliu@stcloudstate.edu>
2017-08-16 12:24:43 -0500

    see if it works

--- src/components/Hello.vue
+++ src/components/Hello.vue
@@ -19,7 +19,7 @@ export default {
   name: 'hello',
   data () {
     return {
-      msg: 'Sucessfully install the vue111',
+      msg: 'Want to see if it works',
       Info:{
         input1:'',
         input2:''
